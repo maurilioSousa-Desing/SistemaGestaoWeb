fafb519e529b9bf605c9c4c733d2b2514ffe606b
maurilioSousa-Desing <fmaurilio6@gmail.com>
2021-07-18 13:36:08 -0300

    Create SistemaGestaoWebApplicationTests.java

---
+++ src/test/java/com/sistema/sof/SistemaGestaoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.sof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestaoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
